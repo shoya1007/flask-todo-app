20e479d35e0d227b72012e51ea0fe85137efd1b3
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2021-02-01 16:54:30 +0900

    タスクの追加、参照機能を実装

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 #下記によってtodo.dbという名前のデータベースを設定
@@ -13,10 +14,32 @@ class Post(db.Model):
     detail = db.Column(db.String(100))
     due = db.Column(db.DateTime, nullable=False)
     
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+    
+    else:
+        #POSTされた内容を受け取る
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
+        
+        #受け取った文字型の日付を日付型に変換
+        due = datetime.datetime.strptime(due, '%Y-%m-%d')
+        #Postクラスに受け取った内容を渡す
+        new_post = Post(title=title, detail=detail, due=due)
+        
+        #dbに内容を追加
+        db.session.add(new_post)
+        #追加した内容を保存
+        db.session.commit()
+        return redirect('/')
 
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
