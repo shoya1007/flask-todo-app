b2081585bc3f85a342b6c3438f8927a34534975f
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2021-02-01 16:01:35 +0900

    データベース作成

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#下記によってtodo.dbという名前のデータベースを設定
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+#dbを生成
+db=SQLAlchemy(app)
 
-
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+    
 @app.route('/')
 def index():
     return render_template('index.html')
