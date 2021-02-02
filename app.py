ebe11e30c4b4f1d58747b31bcb035c6264621411
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2021-02-02 14:37:28 +0900

    タスクの削除機能、更新機能を追加

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Post(db.Model):
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'GET':
-        posts = Post.query.all()
+        posts = Post.query.order_by(Post.due).all()
         return render_template('index.html', posts=posts)
     
     else:
@@ -45,6 +45,27 @@ def create():
 def read(id):
     post = Post.query.get(id)
     return render_template('detail.html', post=post)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Post.query.get(id)
     
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.title = request.form.get('title')
+        post.detail = request.form.get('detail')
+        post.due = datetime.datetime.strptime(request.form.get('due'), '%Y-%m-%d')
+        
+        db.session.commit()
+        return redirect('/')
+
 if __name__ == '__main__':
     app.run(debug=True)
